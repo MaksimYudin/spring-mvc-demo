9db4b7c52932501654234d6d07c45a31a0d690dc
Maksim <metalman@inbox.ru>
2020-10-23 01:06:33 +0300

    Add validation min, max, regex. Add preprocessing(InitBinder) in controller

--- src/main/java/ru/yudin/springdemo/controllers/CustomerController.java
+++ src/main/java/ru/yudin/springdemo/controllers/CustomerController.java
@@ -1,9 +1,12 @@
 package ru.yudin.springdemo.controllers;
 
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import ru.yudin.springdemo.models.Customer;
@@ -13,6 +16,17 @@ import javax.validation.Valid;
 @Controller
 @RequestMapping("/customer")
 public class CustomerController {
+
+    // preprocess every String form data
+    // Trim string, if string is empty --> null
+    @InitBinder
+    public void initBinder(WebDataBinder dataBinder) {
+
+        StringTrimmerEditor stringTrimmerEditor = new StringTrimmerEditor(true);
+
+        dataBinder.registerCustomEditor(String.class, stringTrimmerEditor);
+
+    }
 
     @GetMapping("/showForm")
     public String showForm(Model model) {
--- src/main/java/ru/yudin/springdemo/models/Customer.java
+++ src/main/java/ru/yudin/springdemo/models/Customer.java
@@ -1,14 +1,20 @@
 package ru.yudin.springdemo.models;
 
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
+import javax.validation.constraints.*;
 
 public class Customer {
     private String firstName;
 
-    @NotNull
+    @NotNull(message="is required")
     @Size(min=1, message="is required")
     private String lastName;
+
+    @Min(value = 0, message = "must be >= 0")
+    @Max(value = 10, message = "must be <= 10")
+    private int freePass;
+
+    @Pattern(regexp = "^[a-zA-Z0-9]{5}", message = "only 5 chars/digits")
+    private String postalCode;
 
     public String getFirstName() {
         return firstName;
@@ -24,5 +30,21 @@ public class Customer {
 
     public void setLastName(String lastName) {
         this.lastName = lastName;
+    }
+
+    public int getFreePass() {
+        return freePass;
+    }
+
+    public void setFreePass(int freePass) {
+        this.freePass = freePass;
+    }
+
+    public String getPostalCode() {
+        return postalCode;
+    }
+
+    public void setPostalCode(String postalCode) {
+        this.postalCode = postalCode;
     }
 }
