23525cc4d0ceed7af5e7258c47fd3625b153aa93
Maksim <metalman@inbox.ru>
2020-10-26 17:18:32 +0300

    Hibenate. 1 to 1 UNI relationship, create, delete, cascade

---
+++ src/main/java/ru/yudin/hibernate/demo/CreateInstructorDemo.java
@@ -0,0 +1,44 @@
+package ru.yudin.hibernate.demo;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import ru.yudin.hibernate.demo.entity.Instructor;
+import ru.yudin.hibernate.demo.entity.InstructorDetail;
+import ru.yudin.hibernate.demo.entity.Student;
+
+
+public class CreateInstructorDemo {
+    public static void main(String[] args) {
+
+        SessionFactory sessionFactory = new Configuration()
+                                        .configure("hibernate_1to1uni.cfg.xml")
+                                        .addAnnotatedClass(Instructor.class)
+                                        .addAnnotatedClass(InstructorDetail.class)
+                                        .buildSessionFactory();
+
+        Session session = sessionFactory.getCurrentSession();
+
+        try {
+
+            InstructorDetail instructorDetail =
+                    new InstructorDetail("www.youtube.com", "it");
+
+            Instructor instructor = new Instructor("max", "yudin", "do@email.com");
+
+            instructor.setInstructorDetail(instructorDetail);
+
+            session.beginTransaction();
+
+            session.save(instructor);
+
+            session.getTransaction().commit();
+
+        } finally {
+            sessionFactory.close();
+        }
+
+    }
+
+
+}
---
+++ src/main/java/ru/yudin/hibernate/demo/DeleteInstructorDemo.java
@@ -0,0 +1,41 @@
+package ru.yudin.hibernate.demo;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import ru.yudin.hibernate.demo.entity.Instructor;
+import ru.yudin.hibernate.demo.entity.InstructorDetail;
+
+
+public class DeleteInstructorDemo {
+    public static void main(String[] args) {
+
+        SessionFactory sessionFactory = new Configuration()
+                                        .configure("hibernate_1to1uni.cfg.xml")
+                                        .addAnnotatedClass(Instructor.class)
+                                        .addAnnotatedClass(InstructorDetail.class)
+                                        .buildSessionFactory();
+
+        Session session = sessionFactory.getCurrentSession();
+
+        try {
+
+            int id = 1;
+
+            session.beginTransaction();
+
+            Instructor instructor = session.get(Instructor.class, id);
+
+            if (instructor != null)
+                session.delete(instructor);
+
+            session.getTransaction().commit();
+
+        } finally {
+            sessionFactory.close();
+        }
+
+    }
+
+
+}
